bf7ee911b791ba8af7cf03126f2922dae8ae3b91
Begunov Dmitry <begdv@yandex.ru>
2022-09-17 22:13:19 +0300

    Добавить компонент приложения App

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import logo from '../images/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Header App__header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
