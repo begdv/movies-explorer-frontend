afe7323d3c7ff94d9c9edc34950522badf1fd1be
Begunov Dmitry <begdv@yandex.ru>
2022-10-15 10:21:05 +0300

    Исправить ошибки перед ревью

--- src/components/pages/NotFound/NotFound.js
+++ src/components/pages/NotFound/NotFound.js
@@ -1,8 +1,17 @@
-import MELink from '../../controls/MELink/MELink';
+import { useNavigate } from 'react-router-dom';
+
+import MEButton from '../../controls/MEButton/MEButton';
 
 import './NotFound.css';
 
 function NotFound() {
+  const navigate = useNavigate();
+
+  const handleClickBack = (e) => {
+    e.preventDefault();
+
+    navigate(-1);
+  }
   return (
     <main className="NotFound">
       <section className="NotFound__content">
@@ -11,7 +20,7 @@ function NotFound() {
           <span className="NotFound__description">Страница не найдена</span>
         </div>
         <div className='NotFound__link'>
-          <MELink className="MELink_type_signing" to="/" title="Назад"/>
+          <MEButton className="MEButton_type_not-found" onClick={handleClickBack} title="Назад"/>
         </div>
       </section>
     </main>
